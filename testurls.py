42d3f04a6a65fc7fc3fc7e6635a01eea20d6bdbd
aluusre <andrew.luu@digicert.com>
2025-10-23 15:50:30 -0700

    change json url

--- testurls.py
+++ testurls.py
@@ -1,6 +1,7 @@
 import requests
 
-url = "https://raw.githubusercontent.com/user/repo/main/data.json"
+#url = "https://raw.githubusercontent.com/user/repo/main/data.json"
+url = "https://github.com/aluusre/CRLproject/blob/main/CRLurls2.json"
 response = requests.get(url)
 data = response.json()  # Parse JSON directly
 
