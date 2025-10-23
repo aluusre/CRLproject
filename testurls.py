9e2801369bac521b859411963d50ad41b917f6d9
aluusre <andrew.luu@digicert.com>
2025-10-23 16:26:11 -0700

    tested good

--- testurls.py
+++ testurls.py
@@ -3,7 +3,7 @@
 
 import requests
 
-url = "https://raw.githubusercontent.com/aluusre/CRLproject/main/CRLurls2.json"
+url = "https://raw.githubusercontent.com/aluusre/CRLproject/main/CRLURLs.json"
 response = requests.get(url)
 
 print("HTTP Status:", response.status_code)
@@ -16,3 +16,13 @@ if response.status_code == 200:
         print("Response is not valid JSON. Raw text:\n", response.text)
 else:
     print("Failed to fetch JSON. Raw response:\n", response.text)
+
+
+
+"""
+DC-C02G10ZDMD6T CRLproject % python3 testurls.py 
+/Users/Library/Python/3.9/lib/python/site-packages/urllib3/__init__.py:35: NotOpenSSLWarning: urllib3 v2 only supports OpenSSL 1.1.1+, currently the 'ssl' module is compiled with 'LibreSSL 2.8.3'. See: https://github.com/urllib3/urllib3/issues/3020
+  warnings.warn(
+HTTP Status: 200
+JSON Data: {'eventType': 'CDNCheck', 'type': 'crl', 'urls': ['http://crl.digicert.cn/DigiCertAssuredIDRootCA.crl', 'http://crl3.digicert.com/DigiCertGlobalRootCA.crl']}
+"""
